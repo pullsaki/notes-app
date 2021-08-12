6d3f43922dfebb36d2fd5fd70ec3644827b91fda
Saketh Pulluri <saketh.pulluri2002@gmail.com>
2021-08-12 21:39:00 +0530

    Tryout typography and button components

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Button, Container, Typography } from "@material-ui/core";
+import React from "react";
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        onClick={() => console.log("Clicked!")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
